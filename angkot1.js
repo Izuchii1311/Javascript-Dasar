2b59f7e9006eabca7a76f7f895da99bc404771e1
LuthfiNRamadhan <strinstring183@gmail.com>
2023-05-14 09:59:15 +0700

    #18 Pengulangan While Pada JavaScript

---
+++ angkot1.js
@@ -0,0 +1,5 @@
+var jumAngkot = 1;
+while(jumAngkot <= 10){
+    // alert('Angkot No ' + jumAngkot++ + ' beroperasi dengan baik');
+    console.log('Angkot No ' + jumAngkot++ + ' beroperasi dengan baik');
+}
